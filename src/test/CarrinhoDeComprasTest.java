9735570c220977c6ec765668e3d53c9f0394b93e
Netson <netsoncavina@gmail.com>
2022-09-01 19:02:10 -0300

    Adição de teste com mais pizzas

--- src/test/CarrinhoDeComprasTest.java
+++ src/test/CarrinhoDeComprasTest.java
@@ -14,7 +14,10 @@ import main.CarrinhoDeCompras;
 class CarrinhoDeComprasTest {
 	
 	Pizza p1 = new Pizza("Queijo","Presunto","Cebola");
+	Pizza p2 = new Pizza("Bacon","Queijo", "Oregano");
 	CarrinhoDeCompras carrinho = new CarrinhoDeCompras(p1);
+	
+	
 	
 	@Test
 	void testGetPizza() {
@@ -26,6 +29,12 @@ class CarrinhoDeComprasTest {
 	@Test
 	void testImprimeTotal() {
 		assertEquals("20",carrinho.imprimeTotal());
+	}
+	
+	@Test
+	void testImprimeTotal2Pizzas() {
+		carrinho.addPizza(p2);
+		assertEquals("40", carrinho.imprimeTotal());
 	}
 	
 }
